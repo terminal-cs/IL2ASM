9877b29c0e87ef1f3be8beee5ea2a7b3586d732b
Evan Lawrence <evanlawrence@cyberfear.com>
2022-05-19 19:55:59 -0700

    some changes

--- IL2ASM/Program.cs
+++ IL2ASM/Program.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics;
 
-namespace IL2ASM {
-
+namespace IL2ASM
+{
     public static class Entry
     {
         public static bool IsLinux
@@ -13,26 +13,21 @@ namespace IL2ASM {
             }
         }
         public static string SP = IsLinux ? "/" : "\\";
-        public static string Root = IsLinux ? "../../../../" : "..\\..\\..\\..\\";
+        public static string Root = $"..{SP}..{SP}..{SP}..{SP}";
         public static string Nasm = IsLinux ? "nasm" : Root + "nasm.exe";
         public static string Qemu = IsLinux ? "qemu-system-i386" : $"C:{SP}Program Files{SP}qemu{SP}qemu-system-i386.exe";
         public static string Input = $"{Root}Kernel{SP}bin{SP}Debug{SP}net6.0{SP}Kernel.dll";
         public static string Output = $"{Root}Binary{SP}Kernel";
 
-
         public static void Main()
         {
-            //linux distros usually dont need a path and can just use the executable name
-            //but windows needs the full path
-            //also the path differs on several linux distros
-            if(!IsLinux) {
             if (!File.Exists(Qemu) && !File.Exists(Nasm))
             {
                 Console.WriteLine("Qemu/nasm not found!");
             }
-            }
-            if(!Directory.Exists($"{Root}Binary{SP}")) {
-            Directory.CreateDirectory($"{Root}Binary{SP}");
+            if (!Directory.Exists($"{Root}Binary{SP}"))
+            {
+                Directory.CreateDirectory($"{Root}Binary{SP}");
             }
             File.WriteAllText(Output + ".asm", Compiler.Compile(Input));
             Process.Start(Nasm, Output + ".asm " + " -o " + Output + $".bin -IBinary{SP}Libraries{SP}");
