5006c52a0bbcaf9dd2fac8a2a19452d8406cc88a
Evan Lawrence <evanlawrence@cyberfear.com>
2022-05-19 19:36:23 -0700

    minor fix

--- IL2ASM/Program.cs
+++ IL2ASM/Program.cs
@@ -12,18 +12,19 @@ namespace IL2ASM {
                 return (p == 4) || (p == 6) || (p == 128);
             }
         }
+        public static string SP = IsLinux ? "/" : "\\";
         public static string Root = IsLinux ? "../../../../" : "..\\..\\..\\..\\";
         public static string Nasm = IsLinux ? "/bin/nasm" : Root + "nasm.exe";
-        public static string Qemu = IsLinux ? "/bin/qemu" : "C:\\Program Files\\qemu\\qemu-system-i386.exe";
-        public static string Input = $"{Root}Kernel\\bin\\Debug\\net6.0\\Kernel.dll";
-        public static string Output = $"{Root}Binary\\Kernel";
+        public static string Qemu = IsLinux ? "/bin/qemu" : $"C:{SP}Program Files{SP}qemu{SP}qemu-system-i386.exe";
+        public static string Input = $"{Root}Kernel{SP}bin{SP}Debug{SP}net6.0{SP}Kernel.dll";
+        public static string Output = $"{Root}Binary{SP}Kernel";
 
         public static void Main() {
 
-            Directory.CreateDirectory($"{Root}Binary\\");
+            Directory.CreateDirectory($"{Root}Binary{SP}");
             File.WriteAllText(Output + ".asm", Compiler.Compile(Input));
-            Process.Start(Nasm, Output + ".asm " + " -o " + Output + ".bin -IBinary\\Libraries\\");
-            Process.Start(Qemu, $"{Root}Binary\\Kernel.bin");
+            Process.Start(Nasm, Output + ".asm " + " -o " + Output + $".bin -IBinary{SP}Libraries{SP}");
+            Process.Start(Qemu, $"{Root}Binary{SP}Kernel.bin");
         }
     }
 }
