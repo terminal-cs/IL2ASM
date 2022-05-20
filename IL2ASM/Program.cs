4deefad3bbe4b0d08fccbb97791cec735751a323
KM198912 <55886806+KM198912@users.noreply.github.com>
2022-05-20 04:44:06 +0200

    Small Changes and linux fixes

--- IL2ASM/Program.cs
+++ IL2ASM/Program.cs
@@ -14,14 +14,15 @@ namespace IL2ASM {
         }
         public static string SP = IsLinux ? "/" : "\\";
         public static string Root = IsLinux ? "../../../../" : "..\\..\\..\\..\\";
-        public static string Nasm = IsLinux ? "/bin/nasm" : Root + "nasm.exe";
-        public static string Qemu = IsLinux ? "/bin/qemu" : $"C:{SP}Program Files{SP}qemu{SP}qemu-system-i386.exe";
+        public static string Nasm = IsLinux ? "nasm" : Root + "nasm.exe";
+        public static string Qemu = IsLinux ? "qemu-system-i386" : $"C:{SP}Program Files{SP}qemu{SP}qemu-system-i386.exe";
         public static string Input = $"{Root}Kernel{SP}bin{SP}Debug{SP}net6.0{SP}Kernel.dll";
         public static string Output = $"{Root}Binary{SP}Kernel";
 
         public static void Main() {
-
+            if(!Directory.Exists($"{Root}Binary{SP}")) {
             Directory.CreateDirectory($"{Root}Binary{SP}");
+            }
             File.WriteAllText(Output + ".asm", Compiler.Compile(Input));
             Process.Start(Nasm, Output + ".asm " + " -o " + Output + $".bin -IBinary{SP}Libraries{SP}");
             Process.Start(Qemu, $"{Root}Binary{SP}Kernel.bin");
